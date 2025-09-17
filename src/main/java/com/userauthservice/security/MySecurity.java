173d22406d709309df484d98979c85af3d998689
Shubham Dubey <shubhamdubey2812@gmail.com>
2025-09-17 11:23:13 +0530

    This class is design custom Security

---
+++ src/main/java/com/userauthservice/security/MySecurity.java
@@ -0,0 +1,51 @@
+package com.userauthservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.AlreadyBuiltException;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.userauthservice.exception.UnauthorizedException;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class MySecurity {
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        try {
+            httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/public/**", "/auth/**").permitAll()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+//                    .anyRequest().authenticated()
+                );
+
+            return httpSecurity.build();
+
+        } catch (IllegalStateException e) {
+            throw new AlreadyBuiltException("This object has already been built");
+        } catch (Exception e) {
+            throw new UnauthorizedException("Invalid token");
+        }
+    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+		
+	}
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+		
+	}
+}
