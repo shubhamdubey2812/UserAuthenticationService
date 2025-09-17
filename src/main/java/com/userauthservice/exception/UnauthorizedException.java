9e2733be9f03e32d20db75f7a9d201595ba5c131
Shubham Dubey <shubhamdubey2812@gmail.com>
2025-09-17 11:24:15 +0530

    Custom Unauth Exception

---
+++ src/main/java/com/userauthservice/exception/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package com.userauthservice.exception;
+
+public class UnauthorizedException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnauthorizedException(String msg) {
+		super(msg);
+	}
+
+}
