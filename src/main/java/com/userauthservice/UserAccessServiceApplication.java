b0c80356f168a31273d9c332e0dbf5d11147f08c
Shubham Dubey <shubhamdubey2812@gmail.com>
2025-09-17 10:36:54 +0530

    Initial commit

---
+++ src/main/java/com/userauthservice/UserAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.userauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userauthservice/UserAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
